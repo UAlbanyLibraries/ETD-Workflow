b1c7b535f3ee9e8a74a95596dbf97333998e1b13
kmules1 <50674690+kmules1@users.noreply.github.com>
2022-01-26 09:42:47 -0500

    correcting to data structure received

--- checkSIP.py
+++ checkSIP.py
@@ -10,13 +10,13 @@ if os.name == "nt":
 else:
     incomingPath = "/media/Library/ETDs"
 
-testSIP = os.path.join(incomingPath, "storage", "SIP", "etdadmin_upload_739987")
+testSIP = os.path.join(incomingPath, "storage", "SIP", "etdadmin_upload_7545")
 SIP = SubmissionInformationPackage()
 SIP.load(testSIP)
 
 print (SIP.identifier)
-print (SIP.bag.info['Author'])
-print (SIP.bag.info['Author-Email'])
+print (SIP.bag.info['Author-1_lname'] + ', ' + SIP.bag.info['Author-1_fname'] + ' ' + SIP.bag.info['Author-1_mname'])
+print (SIP.bag.info['Author-1_email_current'])
 print (SIP.bag.info['Title'])
 print (SIP.bag.info['Completion-Date'])
 print (SIP.identifier)
